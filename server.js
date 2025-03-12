4a579fdc795e3c22c14fae0c9af41a621648a83c
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-12 16:06:33 +0100

    add servers starts commands & add first route

--- server.js
+++ server.js
@@ -2,4 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//middlewere for static files
 app.use(express.static('Public'));
+
+//this turns on the server and starts to listen
+app.listen(port, () =>{
+    console.log(`Server is running on port http://localhost:${port}`);
+})
+
+
+app.get('/', (req, res) => {
+
+    res.send('<h1> Server del mio Blog</h1>');
+
+})
